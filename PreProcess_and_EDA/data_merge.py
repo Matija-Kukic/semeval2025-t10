9bd3a07303f11458c7edb3ce93b1de1e41928e95
Matija Kukic <matija-luka.kukic@fer.hr>
2024-12-01 18:01:33 +0100

    initial commit, data for subtask one merged

---
+++ PreProcess_and_EDA/data_merge.py
@@ -0,0 +1,41 @@
+import os
+import pandas as pd
+
+os.chdir("../data/train_data")
+data_dir = os.getcwd()
+# print(data_dir)
+
+columns = ["lang", "art_name", "entity", "start", "end", "class1", "classes2"]
+new_data = list()
+for dr in os.listdir():
+    print(dr)
+    os.chdir(dr)
+    file_path = str(os.getcwd()) + "/subtask-1-annotations.txt"
+    print(file_path)
+    with open(file_path, "r") as file:
+        content = file.read()
+    rows = content.split("\n")
+    for r in rows:
+        a = r.split("\t")
+        # print(a)
+        if a != [""]:
+            d = dict()
+            d["lang"] = str(dr)
+            d["art_name"] = a[0]
+            d["entity"] = a[1]
+            d["start"] = a[2]
+            d["end"] = a[3]
+            d["class1"] = a[4]
+            d["classes2"] = [i for i in a[5:]]
+            file_path2 = str(os.getcwd()) + "/raw-documents/" + a[0]
+            with open(file_path2, "r") as file2:
+                content2 = file2.read()
+            d["text"] = content2
+            # print(d)
+            new_data.append(d)
+    os.chdir("..")
+df = pd.DataFrame(new_data)
+print(df.head())
+os.chdir("..")
+os.chdir("../merged_data")
+df.to_parquet("subtask1.parquet", index=False)
